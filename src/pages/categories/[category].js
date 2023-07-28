a9ae9950d9f01a1a202950ebf04268c77e65647f
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-29 01:37:57 +0600

    fix: single category page bug fix

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -1,18 +1,14 @@
-import { addComponent } from "@/redux/pcBuilderSlice/pcBuilderSlice";
+/* eslint-disable @next/next/no-img-element */
+import RootLayout from "@/components/layouts/RootLayout";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { AiFillStar } from "react-icons/ai";
 
-const ComponentsPage = () => {
-  const dispatch = useDispatch();
+const CategoriesPage = () => {
   const router = useRouter();
   console.log(router?.query?.category);
-
-  const handleAddToBuilder = (product) => {
-    dispatch(addComponent(product));
-    router.push("/pc-builder");
-  };
 
   const featuredProducts = [
     {
@@ -20,7 +16,7 @@ const ComponentsPage = () => {
       image:
         "https://images.unsplash.com/photo-1551269901-5c5e14c25df7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhvZ3dhcnRzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       productName: "Product 1",
-      category: "CPU - Processor",
+      category: "CPU / Processor",
       price: 199.99,
       status: "In Stock",
       rating: 4.5,
@@ -186,83 +182,50 @@ const ComponentsPage = () => {
   ];
 
   return (
-    <div className="min-h-screen text-black">
-      <div className="w-full sm:w-3/4 mx-auto">
-        {featuredProducts?.map((product) => (
-          <div key={product?.id} className="p-4   w-full">
-            <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-4 sm:flex-row flex-col">
-              <div className="w-full h-40 sm:w-36 sm:h-36 sm:mr-4 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-lg bg-indigo-100 text-indigo-500 flex-shrink-0">
-                <Image
-                  src={product?.image}
-                  className="w-full h-full rounded-lg"
-                  width={300}
-                  height={300}
-                  alt=""
-                />
-              </div>
-              <div className="flex-grow ">
-                <h2 className="text-gray-900 text-lg title-font font-medium  ">
-                  {product?.productName}
-                </h2>
-                <h2 className="text-gray-900 text-base title-font font-medium  mb-4">
-                  {product?.category}
-                </h2>
-                <div className="flex mb-1">
-                  <span className="flex items-center">
-                    <span className="text-gray-600 mr-3">
-                      {product?.rating} Stars
+    <>
+      <div className="min-h-screen mb-16">
+        <div className="container mx-auto bg-white pt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+          {featuredProducts?.map((product) => (
+            <Link key={product?.id} href={`/product/${product?.id}`}>
+              <div className="card w-full glass hover:bg-gray-100">
+                <figure>
+                  <Image
+                    src={product?.image}
+                    alt="product image"
+                    width={300}
+                    height={300}
+                    responsive="true"
+                  />
+                </figure>
+                <div className="card-body p-4">
+                  <h2 className="card-title">{product?.productName}</h2>
+                  <p>
+                    <span className="font-semibold">Category:</span>{" "}
+                    {product?.category}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Status:</span>{" "}
+                    {product?.status}
+                  </p>
+                  <div className="flex justify-between">
+                    <span className="flex items-center mr-1 font-bold">
+                      {product?.rating}
+                      <AiFillStar className="ml-1 text-orange-500" />
                     </span>
-                    {new Array(Math.floor(product?.rating))
-                      .fill(null)
-                      .map((star, i) => (
-                        <svg
-                          key={i}
-                          fill="currentColor"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          className="w-4 h-4 text-green-500"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                        </svg>
-                      ))}
-                    {new Array(5 - Math.floor(product?.rating))
-                      .fill(null)
-                      .map((star, i) => (
-                        <svg
-                          key={i}
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="w-4 h-4 text-green-500"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                        </svg>
-                      ))}
-                  </span>
+                    <span className="font-bold">${product?.price}</span>
+                  </div>
                 </div>
-                <p className="text-base">Price: ${product?.price}</p>
-                <p className="text-base">Status: {product?.status}</p>
               </div>
-              <div className="flex items-center">
-                <button
-                  onClick={() => handleAddToBuilder(product)}
-                  className="mt-3 sm:mt-0 w-full btn btn-accent text-white"
-                >
-                  Add To Builder
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default ComponentsPage;
+export default CategoriesPage;
+
+CategoriesPage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
