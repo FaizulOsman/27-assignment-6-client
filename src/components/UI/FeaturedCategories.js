152f774da90cd68f261a04f824900ad375aa4e89
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-29 16:43:40 +0600

    feat: featuredCategories link added

--- src/components/UI/FeaturedCategories.js
+++ src/components/UI/FeaturedCategories.js
@@ -1,12 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FeaturedCategories = () => {
   const featuredCategories = [
     {
       id: "1",
-      category: "CPU / Processor",
+      category: "CPU-Processor",
       image:
         "https://cdn.arstechnica.net/wp-content/uploads/2023/01/intel-13th-gen-desktop-800x450.jpg",
     },
@@ -48,24 +49,23 @@ const FeaturedCategories = () => {
       <div className="container px-5 pt-12 pb-24 mx-auto">
         <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 sm:-m-4 -mx-4 -mb-10 -mt-4">
           {featuredCategories?.map((item) => (
-            <div
-              key={item?.id}
-              className="p-2 w-full sm:mb-0 mb-6 hover:bg-gray-100 hover:scale-105 duration-300 border rounded-lg shadow-md cursor-pointer"
-            >
-              <div className="rounded-lg h-28 overflow-hidden">
-                <Image
-                  src={item?.image}
-                  alt="product category image"
-                  className="object-cover object-center h-full w-full"
-                  width={300}
-                  height={300}
-                  responsive="true"
-                />
+            <Link href={`/categories/${item?.category}`} key={item?.id}>
+              <div className="p-2 w-full sm:mb-0 mb-6 hover:bg-gray-100 hover:scale-105 duration-300 border rounded-lg shadow-md cursor-pointer">
+                <div className="rounded-lg h-28 overflow-hidden">
+                  <Image
+                    src={item?.image}
+                    alt="product category image"
+                    className="object-cover object-center h-full w-full"
+                    width={300}
+                    height={300}
+                    responsive="true"
+                  />
+                </div>
+                <h2 className="text-md font-medium title-font text-center text-gray-900 mt-2">
+                  {item?.category}
+                </h2>
               </div>
-              <h2 className="text-md font-medium title-font text-center text-gray-900 mt-2">
-                {item?.category}
-              </h2>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
